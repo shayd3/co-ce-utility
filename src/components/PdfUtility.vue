00805d24b9bedb8e6a6c6f2d4f968232db44ef5f
shayd3 <ryan.ross215@gmail.com>
2023-07-26 22:41:09 -0400

    PdfUtility.vue init Clean up some boilerplate Add pdf-js Add PrimeVue UI/Component Framework

---
+++ src/components/PdfUtility.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import Panel from 'primevue/panel';
+
+</script>
+
+<template>
+    <div id="pdfPanels">
+        <Panel header="PDF">
+            <p>PDF Details and Button Here</p>
+        </Panel>
+        <Panel header="Signature">
+            <p>Signature Details and Button Here</p>
+        </Panel>
+    </div>
+</template>
+
+<style scoped>
+#pdfPanels {
+    display: flex;
+    flex-direction: row;
+    gap: 10px;
+
+}
+</style>
+```
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+// import TheWelcome from '../components/TheWelcome.vue'
+import PdfUtility from '../components/PdfUtility.vue'
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <!-- <TheWelcome /> -->
+    <PdfUtility />
   </main>
 </template>
