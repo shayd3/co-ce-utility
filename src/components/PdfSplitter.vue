177d3bc2c7cef89fcccfcb18cdbd6b04c9f07121
shayd3 <ryan.ross215@gmail.com>
2023-07-28 10:55:26 -0400

    Get lines init

--- src/components/PdfSplitter.vue
+++ src/components/PdfSplitter.vue
@@ -1,10 +1,21 @@
 <script setup lang="ts">
 import { usePdfStore } from '@/stores/pdf';
+import * as PDFJS from 'pdfjs-dist/legacy/build/pdf';
+import { PDFDocument } from 'pdf-lib';
 
+const store = usePdfStore();
+
+// Get each line from first page of PDF
+const getLines = async () => {
+
+}
 </script>
 
 <template>
     <div>
-        I'm a pdf splitter
+        <!-- Display lines from getLines-->
+        <div v-for="line in getLines" :key="line">
+            {{ line }}
+        </div>
     </div>
 </template>
