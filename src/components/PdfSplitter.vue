60372f6012e780d8cbbb9030411fdce62e8df3f0
shayd3 <ryan.ross215@gmail.com>
2023-07-28 11:22:59 -0400

    attempting to get PDFJS working

--- src/components/PdfSplitter.vue
+++ src/components/PdfSplitter.vue
@@ -1,12 +1,22 @@
 <script setup lang="ts">
 import { usePdfStore } from '@/stores/pdf';
-import * as PDFJS from 'pdfjs-dist/legacy/build/pdf';
+// TODO: Get PDFJS working to extract text/lines...
+import * as PDFJS from "pdfjs-dist/legacy/build/pdf";
 import { PDFDocument } from 'pdf-lib';
 
 const store = usePdfStore();
 
 // Get each line from first page of PDF
 const getLines = async () => {
+    let pdf = store.getPdf()
+    let pdfBytes = store.getPdfBytes();
+    let pdfName = store.getPdfName();
+
+    // If PDF is not loaded, return
+    if (!pdf) {
+        return;
+    }
+
 
 }
 </script>
