f71d9323b462d6d086c7d8cead304f5480c10967
shayd3 <ryan.ross215@gmail.com>
2023-07-28 08:33:42 -0400

    Add DialogService plugin

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import PrimeVue from 'primevue/config';
-
+import DialogService from 'primevue/dialogservice';
 
 import App from './App.vue'
 import router from './router'
@@ -17,5 +17,6 @@ const app = createApp(App)
 app.use(createPinia())
 app.use(router)
 app.use(PrimeVue)
+app.use(DialogService)
 
 app.mount('#app')
