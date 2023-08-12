96da31731f5543f82294b9c756ed8fd13d25ec54
shayd3 <ryan.ross215@gmail.com>
2023-08-12 09:13:08 -0400

    Add text content async function

--- src/components/PdfSplitter.vue
+++ src/components/PdfSplitter.vue
@@ -1,14 +1,15 @@
 <script setup lang="ts">
 import { usePdfStore } from '@/stores/pdf';
-import * as PDFJS from "pdfjs-dist/legacy/build/pdf";
 import type { TextItem } from 'pdfjs-dist/types/src/display/api';
+import { ref } from 'vue';
+import * as PDFJS from "pdfjs-dist";
 PDFJS.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${PDFJS.version}/pdf.worker.js`;
 
-
 const store = usePdfStore();
+const firstPageContent = ref([]) as { value: string[] };
 
 // Get each line from first page of PDF
-const getLines = async () => {
+(async () => {
     let pdfBytes = store.getPdfBytes();
 
     if (!pdfBytes) return;
@@ -18,16 +19,19 @@ const getLines = async () => {
     let page = await pdfDoc.getPage(1);
     let textContent = await page.getTextContent();
     let lines = textContent.items.map((item) => (item as TextItem).str);
-    console.log(lines)
-    // Return lines
-    return lines;
+
+    firstPageContent.value = lines;
+})();
+
+const getFirstPageContent = () => {
+    return firstPageContent.value;
 }
 </script>
 
 <template>
     <div>
         <p>PDF: {{ store.getPdfName() }}</p>
-        <p v-for="line in getLines()" :key="line.toString()">
+        <p v-for="line in getFirstPageContent()" :key="line.toString()">
             {{ line }}
         </p>
     </div>
