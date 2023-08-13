6af6d269bd2831e14a4b82752cd496d396023346
shayd3 <ryan.ross215@gmail.com>
2023-08-13 07:46:54 -0400

    Get Listbox added to modal

--- src/components/PdfSplitter.vue
+++ src/components/PdfSplitter.vue
@@ -4,8 +4,11 @@ import type { TextItem } from 'pdfjs-dist/types/src/display/api';
 import { ref } from 'vue';
 import * as PDFJS from "pdfjs-dist";
 
+import Listbox from 'primevue/listbox';
+
 const store = usePdfStore();
 const firstPageContent = ref([]) as { value: string[] };
+const selectedLine = ref();
 
 // Get each line from first page of PDF
 (async () => {
@@ -25,6 +28,19 @@ const getFirstPageContent = () => {
     return firstPageContent.value;
 }
 
+const cleanUpPageContent = (pageContent: string[]) => {
+    // Remove empty lines
+    let cleanedPageContent = pageContent.filter((line) => line.trim() !== "");
+
+    // Remove lines that is just 1 or more '_'
+    cleanedPageContent = cleanedPageContent.filter((line) => !line.match(/^_+$/));
+
+    // Remove lines that are just integers, decimals, or integers with decimals
+    cleanedPageContent = cleanedPageContent.filter((line) => !line.match(/^\d+\.?\d*$/));
+
+    return cleanedPageContent;
+}
+
 const getPdfFileName = () => {
     let pdfFile = store.getPdfFile();
     return pdfFile?.name;
@@ -33,9 +49,7 @@ const getPdfFileName = () => {
 
 <template>
     <div>
-        <p>PDF: {{ getPdfFileName() }}</p>
-        <p v-for="line in getFirstPageContent()" :key="line.toString()">
-            {{ line }}
-        </p>
+        <p><b>PDF:</b> {{ getPdfFileName() }}</p>
+        <Listbox v-model="selectedLine" :options="cleanUpPageContent(getFirstPageContent())" class="w-full" />
     </div>
 </template>
