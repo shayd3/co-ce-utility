463f2970a537e20e3960f3ac8089408cf4b7ace0
shayd3 <ryan.ross215@gmail.com>
2023-08-22 20:50:32 -0400

    Getting zip file working

--- src/components/PdfSplitterDialogFooter.vue
+++ src/components/PdfSplitterDialogFooter.vue
@@ -1,17 +1,61 @@
 <script setup lang="ts">
 import { inject, ref } from "vue";
 import { usePdfStore } from '@/stores/pdf';
+import JSZip from 'jszip';
+import * as fs from 'file-saver';
 import Button from 'primevue/button';
 import Checkbox from 'primevue/checkbox';
+import { PDFDocument } from "pdf-lib";
+import { compileScript } from "vue/compiler-sfc";
 
 const dialogRef = inject("dialogRef") as any;
 const formatName = ref(true);
 const store = usePdfStore();
 
-const closeDialog = (e : any) => {
-    e.formatName = formatName.value;
-    dialogRef.value.close(e);
+// TODO: Split the PDF on closeDialog and then download
+const closeDialog = () => {
+    splitPdf();
+    dialogRef.value.close();
 };
+
+// Create a new PDF for each page in the PDF
+const splitPdf = () => {
+    let pdfBytes = store.getPdfBytes();
+    let zipFile = new JSZip();
+
+    pdfBytes.then((arrayBuffer) => {
+        let pdfBytes = new Uint8Array(arrayBuffer!);
+        let pdfDoc = PDFDocument.load(pdfBytes, {
+            updateMetadata: false
+        });
+        pdfDoc.then((doc) => {
+            let pages = doc.getPages();
+            pages.forEach((page, index) => {
+                let newPdf = PDFDocument.create();
+                newPdf.then((newDoc) => {
+                    const copiedPage = newDoc.copyPages(doc, [index]);
+                    copiedPage.then((copiedPage) => {
+                        newDoc.addPage(copiedPage[0]);
+                        let newPdfBytes = newDoc.save();
+                        newPdfBytes.then((newBytes) => {
+                            console.log(newBytes)
+                            zipFile.file(`page${index}.pdf`, newBytes);
+                        });
+                    });
+                });
+            });
+        });
+    });
+
+    // TODO: Get zipping working
+    zipFile.generateAsync({type:"blob"}).then((content) => {
+        // Save the zip file
+        console.log(content)
+        fs.saveAs(content, "splitPDF.zip");
+    })
+
+}
+
 </script>
 
 <template>
