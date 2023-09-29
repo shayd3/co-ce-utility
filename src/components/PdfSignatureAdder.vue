3f220385afe417b23d5ff4dda91c8baae97638f0
shayd3 <ryan.ross215@gmail.com>
2023-09-29 08:22:46 -0400

    Get square drawing working without clearing out the pdf page!

--- src/components/PdfSignatureAdder.vue
+++ src/components/PdfSignatureAdder.vue
@@ -13,6 +13,7 @@ import { ref, onMounted } from 'vue';
 import { usePdfStore } from '@/stores/pdf';
 import { useSignatureStore } from '@/stores/signature';
 import * as PDFJS from "pdfjs-dist";
+import { PDFContext } from 'pdf-lib';
 
 const pdfStore = usePdfStore();
 const signatureStore = useSignatureStore();
@@ -23,6 +24,9 @@ const ToolTips = {
 
 let pdfPage = ref<PDFJS.PDFPageProxy | null>();
 let pdfCanvas = ref<CanvasRenderingContext2D | null>();
+let pdfImage = ref<HTMLImageElement | undefined>();
+let startX = 0;
+let startY = 0;
 
 onMounted(async () => {
     let pdfArrayBuffer = await pdfStore.getPdfBytes();
@@ -38,13 +42,53 @@ onMounted(async () => {
     canvas.height = viewport.height;
     canvas.width = viewport.width;
 
-    pdfCanvas.value!.fillStyle = "#000000";
-    pdfCanvas.value!.globalAlpha = 0.3;
-
     pdfPageProxy.render({ canvasContext: pdfCanvas.value, viewport: viewport })
 });
 
-// Have
+const saveCanvasState = () => {
+    if (pdfCanvas.value) {
+        console.log("Saving state...")
+        var canvasPic = new Image();
+        canvasPic.src = pdfCanvas.value!.canvas.toDataURL("image/png");
+        pdfImage.value = canvasPic;
+    }
+}
+
+const handleMouseDown = (event: MouseEvent) => {
+    if (pdfImage.value === undefined) {
+        saveCanvasState();
+    }
+    event.preventDefault();
+    event.stopPropagation();
+    startX = event.offsetX;
+    startY = event.offsetY;
+}
+const handleMouseUp = (event: MouseEvent) => {
+    let endX = event.offsetX;
+    let endY = event.offsetY;
+    let width = endX - startX;
+    let height = endY - startY;
+    event.preventDefault();
+    event.stopPropagation();
+    pdfCanvas.value!.strokeRect(startX, startY, width, height);
+}
+const handleMouseMove = (event: MouseEvent) => {
+    if (event.buttons === 1) {
+        let endX = event.offsetX;
+        let endY = event.offsetY;
+        let width = endX - startX;
+        let height = endY - startY;
+        event.preventDefault();
+        event.stopPropagation();
+
+        pdfCanvas.value!.clearRect(0, 0, pdfCanvas.value!.canvas.width, pdfCanvas.value!.canvas.height);
+        pdfCanvas.value!.drawImage(pdfImage.value!, 0, 0);
+        pdfCanvas.value!.beginPath()
+        pdfCanvas.value!.rect(startX, startY, width, height);
+        pdfCanvas.value!.stroke()
+    }
+}
+
 
 
 </script>
@@ -52,7 +96,7 @@ onMounted(async () => {
 <template>
     <div>
         <div class="p-d-flex p-flex-column p-ai-center p-mt-2">
-            <canvas class="border-solid" id="pdf-canvas" ref="pdfCanvas"></canvas>
+            <canvas class="border-solid" id="pdf-canvas" ref="pdfCanvas" v-on:mousedown="handleMouseDown" v-on:mouseup="handleMouseUp" v-on:mousemove="handleMouseMove"></canvas>
         </div>
     </div>
 </template>
